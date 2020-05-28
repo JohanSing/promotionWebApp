ea4b4c190ffda52861b002df548854907fbef34a
Moustanir Soulé-Ali <moustanir.souleali@gmail.com>
2020-05-28 11:03:09 +0200

    feat : add styled-components package

---
+++ src/config/router.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import {
+  Route,
+  BrowserRouter as Router,
+  Redirect,
+  Switch
+} from 'react-router-dom';
+
+import Login from '../components/pages/Login';
+import Home from '../components/pages/Home';
+
+
+const Routes = () => {
+  return (
+    <Router>
+      <Switch>
+        <Route exact path='/' component={Home} />
+        <Route exact path='/login' component={Login} />
+        <Redirect to='/'></Redirect>
+      </Switch>
+    </Router>
+  );
+};
+
+export default Routes;
