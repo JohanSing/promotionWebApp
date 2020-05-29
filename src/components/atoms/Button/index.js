8ed8e69afbcb78483cb223e43d30708085aa6755
Moustanir Soulé-Ali <moustanir.souleali@gmail.com>
2020-05-29 09:15:02 +0200

    feat add button and input component

---
+++ src/components/atoms/Button/index.js
@@ -0,0 +1,70 @@
+import React from 'react';
+import styled from 'styled-components';
+import Proptypes from 'prop-types';
+
+const Button = ({
+  name,
+  borderColor,
+  fillingColor,
+  textColor,
+  textHoverColor,
+  IsInvert,
+  ...props
+}) => {
+  return (
+    <ButtonContainer>
+      <ButtonComponent
+        IsInvert={IsInvert}
+        textHoverColor={textHoverColor}
+        textColor={textColor}
+        fillingColor={fillingColor}
+        borderColor={borderColor}
+        {...props}
+      >
+        {name}
+      </ButtonComponent>
+    </ButtonContainer>
+  );
+};
+const ButtonComponent = styled.button`
+  background-color: ${props =>
+    !props.IsInvert ? `${props.fillingColor}` : 'white'};
+  border: none;
+  color: ${props => props.textColor};
+  padding: 16px 32px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 16px;
+  margin: 4px 2px;
+  transition-duration: 0.4s;
+  cursor: pointer;
+  border: 2px solid ${props => props.borderColor};
+  &:hover {
+    background-color: ${props =>
+      props.IsInvert ? 'white' : props.fillingColor};
+    color: ${props =>
+      props.IsInvert ? props.textHoverColor : props.textColor};
+  }
+`;
+const ButtonContainer = styled.div``;
+
+Button.defaultProps = {
+  name: '',
+  borderColor: 'black',
+  fillingColor: 'black',
+  textHoverColor: 'black',
+  textColor: 'white',
+  IsInvert: false
+};
+
+Button.propTypes = {
+  name: Proptypes.string,
+  borderColor: Proptypes.string,
+  fillingColor: Proptypes.string,
+  textHoverColor: Proptypes.string,
+  textColor: Proptypes.string,
+  IsInvert: Proptypes.bool
+};
+
+export default Button;
---
+++ src/components/atoms/Input/index.js
@@ -0,0 +1,94 @@
+import React from 'react';
+import styled from 'styled-components';
+import PropTypes from 'prop-types';
+
+const FormInput = ({
+  labelName,
+  type,
+  name,
+  id,
+  placeholder,
+  inputColor,
+  ...props
+}) => {
+  return (
+    <InputContainer>
+      <InputComponent
+        type={type}
+        id={id}
+        name={name}
+        placeholder={placeholder}
+        inputColor={inputColor}
+        {...props}
+      />
+      <LabelComponent htmlFor={name}>{labelName}</LabelComponent>
+    </InputContainer>
+  );
+};
+
+const LabelComponent = styled.label`
+  position: absolute;
+  top: 0;
+  display: block;
+  transition: 0.2s;
+  font-size: 12px;
+  color: #9b9b9b;
+`;
+
+const InputComponent = styled.input`
+  font-family: inherit;
+  width: 100%;
+  border: 0;
+  border-bottom: 1px solid #d2d2d2;
+  outline: 0;
+  font-size: 16px;
+  color: #212121;
+  padding: 7px 0;
+  background: transparent;
+  transition: border-color 0.2s;
+  &:focus {
+    padding-bottom: 6px;
+    border-bottom: 2px solid ${props => props.inputColor};
+  }
+  &:placeholder-shown ~ ${LabelComponent} {
+    font-size: 16px;
+    cursor: text;
+    top: 20px;
+  }
+  &:focus ~ ${LabelComponent} {
+    position: absolute;
+    top: 0;
+    display: block;
+    transition: 0.2s;
+    font-size: 0.9em;
+    color: ${props => props.inputColor};
+  }
+  &::placeholder {
+    color: transparent;
+  }
+`;
+
+const InputContainer = styled.div`
+  position: relative;
+  padding: 15px 0 0;
+  margin-top: 10px;
+`;
+FormInput.defaultProps = {
+  labelName: '',
+  type: 'text',
+  name: 'Input',
+  id: '',
+  placeholder: 'Placeholder',
+  inputColor: 'Yellow'
+};
+
+FormInput.propTypes = {
+  labelName: PropTypes.string,
+  type: PropTypes.string,
+  name: PropTypes.string,
+  id: PropTypes.string,
+  placeholder: PropTypes.string,
+  inputColor: PropTypes.string
+};
+
+export default FormInput;
